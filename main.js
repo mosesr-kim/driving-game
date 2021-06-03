a3558c00431d31a274006ebd48f8936a3a027405
Moses Kim <mosesrandykim@gmail.com>
2021-06-03 16:04:51 -0700

    made a function named moveCar that grouped up the code for whenever the car is moving.

--- main.js
+++ main.js
@@ -15,63 +15,43 @@ function controlCar(event) {
     $car.className = 'north';
     car.direction = 'north';
     if (car.moving === true) {
-      clearInterval(car.movingID);
-      var IntervalID = setInterval(moveCarNorth, 16);
-      car.movingID = IntervalID;
-      car.moving = true;
+      movingCar();
     }
   }
   if (event.keyCode === 39) {
     $car.className = 'east';
     car.direction = 'east';
     if (car.moving === true) {
-      clearInterval(car.movingID);
-      var IntervalID = setInterval(moveCarEast, 16);
-      car.movingID = IntervalID;
-      car.moving = true;
+      movingCar();
     }
   }
   if (event.keyCode === 40) {
     $car.className = 'south';
     car.direction = 'south';
     if (car.moving === true) {
-      clearInterval(car.movingID);
-      var IntervalID = setInterval(moveCarSouth, 16);
-      car.movingID = IntervalID;
-      car.moving = true;
+      movingCar();
     }
   }
   if (event.keyCode === 37) {
     $car.className = 'west';
     car.direction = 'west';
     if (car.moving === true) {
-      clearInterval(car.movingID);
-      var IntervalID = setInterval(moveCarWest, 16);
-      car.movingID = IntervalID;
-      car.moving = true;
+      movingCar();
     }
   }
   if (event.keyCode === 32) {
     if (car.moving === false) {
       if (car.direction === 'east') {
-        var IntervalID = setInterval(moveCarEast, 16);
-        car.movingID = IntervalID;
-        car.moving = true;
+        movingCar();
       }
       if (car.direction === 'west') {
-        var IntervalID = setInterval(moveCarWest, 16);
-        car.movingID = IntervalID;
-        car.moving = true;
+        movingCar();
       }
       if (car.direction === 'north') {
-        var IntervalID = setInterval(moveCarNorth, 16);
-        car.movingID = IntervalID;
-        car.moving = true;
+        movingCar();
       }
       if (car.direction === 'south') {
-        var IntervalID = setInterval(moveCarSouth, 16);
-        car.movingID = IntervalID;
-        car.moving = true;
+        movingCar();
       }
     } else {
       clearInterval(car.movingID);
@@ -86,26 +66,32 @@ function carLocation() {
   return car;
 }
 
-function moveCarEast() {
-  var currentLocationX = carLocation().carLeftX;
-  var nextLocationX = currentLocationX + 5 + 'px';
-  $car.style.left = nextLocationX.toString();
+function moveCar() {
+  if (car.direction === 'east') {
+    var currentLocationX = carLocation().carLeftX;
+    var nextLocationX = currentLocationX + 5 + 'px';
+    $car.style.left = nextLocationX.toString();
+  }
+  if (car.direction === 'south') {
+    var currentLocationY = carLocation().carTopY;
+    var nextLocationY = currentLocationY + 5 + 'px';
+    $car.style.top = nextLocationY.toString();
+  }
+  if (car.direction === 'west') {
+    var currentLocationX = carLocation().carLeftX;
+    var nextLocationX = currentLocationX - 5 + 'px';
+    $car.style.left = nextLocationX.toString();
+  }
+  if (car.direction === 'north') {
+    var currentLocationY = carLocation().carTopY;
+    var nextLocationY = currentLocationY - 5 + 'px';
+    $car.style.top = nextLocationY.toString();
+  }
 }
 
-function moveCarWest() {
-  var currentLocationX = carLocation().carLeftX;
-  var nextLocationX = currentLocationX - 5 + 'px';
-  $car.style.left = nextLocationX.toString();
-}
-
-function moveCarNorth() {
-  var currentLocationY = carLocation().carTopY;
-  var nextLocationY = currentLocationY - 5 + 'px';
-  $car.style.top = nextLocationY.toString();
-}
-
-function moveCarSouth() {
-  var currentLocationY = carLocation().carTopY;
-  var nextLocationY = currentLocationY + 5 + 'px';
-  $car.style.top = nextLocationY.toString();
+function movingCar() {
+  clearInterval(car.movingID);
+  var IntervalID = setInterval(moveCar, 16);
+  car.movingID = IntervalID;
+  car.moving = true;
 }
