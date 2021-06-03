ee3d5ac2d78c296c3c9194a8b6f654493f011944
Moses Kim <mosesrandykim@gmail.com>
2021-06-03 14:41:23 -0700

    added an event listener that listens for keydown events and made a function that moves the car if the spacebar is pressed.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var $car = document.querySelector('#car');
 
 window.addEventListener('keydown', turnCar);
+window.addEventListener('keydown', startCar);
 
 function turnCar(event) {
   if (event.keyCode === 38) {
@@ -15,4 +16,27 @@ function turnCar(event) {
   if (event.keyCode === 37) {
     $car.className = 'west';
   }
+}
+
+function startCar(event) {
+  if (event.keyCode === 32) {
+    var intervalID = setInterval(moveCar, 16);
+  }
+}
+
+function carLocation() {
+  var car = {
+    carLeftX: $car.getBoundingClientRect().x,
+    carTopY: $car.getBoundingClientRect().y
+  };
+  return car;
+}
+
+function moveCar() {
+  var currentLocationX = carLocation().carLeftX;
+  // var currentLocationY = carLocation().carTopY;
+  var nextLocationX = currentLocationX + 5 + 'px';
+  // var nextLocationY = currentLocationY + 1 + 'px';
+  $car.style.left = nextLocationX.toString();
+  // $car.style.top = nextLocationY.toString();
 }
