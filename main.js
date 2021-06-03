bc15f9c76feeef54f418ed723ba3cf921b945cc2
Moses Kim <mosesrandykim@gmail.com>
2021-06-03 15:47:31 -0700

    logged which direction the car was facing into the car data model. checked to see which direction the car was facing before resuming to go. also if the car was in motion, the car would move whichever direction the car got turned to.

--- main.js
+++ main.js
@@ -1,36 +1,78 @@
 var $car = document.querySelector('#car');
 
-window.addEventListener('keydown', turnCar);
-window.addEventListener('keydown', startCar);
+window.addEventListener('keydown', controlCar);
 
 var car = {
   carLeftX: 0,
   carTopY: 0,
   moving: false,
-  movingID: 0
+  movingID: 0,
+  direction: 'east'
 };
 
-function turnCar(event) {
+function controlCar(event) {
   if (event.keyCode === 38) {
     $car.className = 'north';
+    car.direction = 'north';
+    if (car.moving === true) {
+      clearInterval(car.movingID);
+      var IntervalID = setInterval(moveCarNorth, 16);
+      car.movingID = IntervalID;
+      car.moving = true;
+    }
   }
   if (event.keyCode === 39) {
-    $car.className = '';
+    $car.className = 'east';
+    car.direction = 'east';
+    if (car.moving === true) {
+      clearInterval(car.movingID);
+      var IntervalID = setInterval(moveCarEast, 16);
+      car.movingID = IntervalID;
+      car.moving = true;
+    }
   }
   if (event.keyCode === 40) {
     $car.className = 'south';
+    car.direction = 'south';
+    if (car.moving === true) {
+      clearInterval(car.movingID);
+      var IntervalID = setInterval(moveCarSouth, 16);
+      car.movingID = IntervalID;
+      car.moving = true;
+    }
   }
   if (event.keyCode === 37) {
     $car.className = 'west';
-  }
-}
-
-function startCar(event) {
-  if (event.keyCode === 32) {
-    if (car.moving === false) {
-      var IntervalID = setInterval(moveCar, 16);
+    car.direction = 'west';
+    if (car.moving === true) {
+      clearInterval(car.movingID);
+      var IntervalID = setInterval(moveCarWest, 16);
       car.movingID = IntervalID;
       car.moving = true;
+    }
+  }
+  if (event.keyCode === 32) {
+    if (car.moving === false) {
+      if (car.direction === 'east') {
+        var IntervalID = setInterval(moveCarEast, 16);
+        car.movingID = IntervalID;
+        car.moving = true;
+      }
+      if (car.direction === 'west') {
+        var IntervalID = setInterval(moveCarWest, 16);
+        car.movingID = IntervalID;
+        car.moving = true;
+      }
+      if (car.direction === 'north') {
+        var IntervalID = setInterval(moveCarNorth, 16);
+        car.movingID = IntervalID;
+        car.moving = true;
+      }
+      if (car.direction === 'south') {
+        var IntervalID = setInterval(moveCarSouth, 16);
+        car.movingID = IntervalID;
+        car.moving = true;
+      }
     } else {
       clearInterval(car.movingID);
       car.moving = false;
@@ -44,11 +86,26 @@ function carLocation() {
   return car;
 }
 
-function moveCar() {
+function moveCarEast() {
   var currentLocationX = carLocation().carLeftX;
-  // var currentLocationY = carLocation().carTopY;
   var nextLocationX = currentLocationX + 5 + 'px';
-  // var nextLocationY = currentLocationY + 1 + 'px';
   $car.style.left = nextLocationX.toString();
-  // $car.style.top = nextLocationY.toString();
+}
+
+function moveCarWest() {
+  var currentLocationX = carLocation().carLeftX;
+  var nextLocationX = currentLocationX - 5 + 'px';
+  $car.style.left = nextLocationX.toString();
+}
+
+function moveCarNorth() {
+  var currentLocationY = carLocation().carTopY;
+  var nextLocationY = currentLocationY - 5 + 'px';
+  $car.style.top = nextLocationY.toString();
+}
+
+function moveCarSouth() {
+  var currentLocationY = carLocation().carTopY;
+  var nextLocationY = currentLocationY + 5 + 'px';
+  $car.style.top = nextLocationY.toString();
 }
